11e618b4d51154d187950c3cf7018f8f9ab7f5a5
Patrick Luca Fazzi <patrick91@live.it>
2020-02-23 16:06:26 +0100

    Add Cache interface

--- spec/KataSpec.php
+++ spec/KataSpec.php
@@ -10,9 +10,9 @@ class KataSpec extends ObjectBehavior
     private const FIZZ_NUMBER = 7;
     private const BUZZ_NUMBER = 11;
 
-    function let()
+    function let(\Pfazzi\FizzBuzzKata\Cache $cache)
     {
-        $this->beConstructedWith(self::FIZZ_NUMBER, self::BUZZ_NUMBER);
+        $this->beConstructedWith($cache, self::FIZZ_NUMBER, self::BUZZ_NUMBER);
     }
 
     function it_is_initializable()
@@ -20,17 +20,17 @@ class KataSpec extends ObjectBehavior
         $this->shouldHaveType(Kata::class);
     }
 
-    function it_returns_fizz_instead_of_the_number_for_multiples_of_three()
+    function it_returns_Fizz_instead_of_the_number_for_multiples_of_three()
     {
         $this->print(self::FIZZ_NUMBER * 2)->shouldReturn('Fizz');
     }
 
-    function it_returns_buzz_instead_of_the_number_for_multiples_of_five()
+    function it_returns_Buzz_instead_of_the_number_for_multiples_of_five()
     {
         $this->print(self::BUZZ_NUMBER * 8)->shouldReturn('Buzz');
     }
 
-    function it_returns_fizz_buzz_instead_of_the_number_which_are_multiples_of_both_three_and_five()
+    function it_returns_FizzBuzz_instead_of_the_number_which_are_multiples_of_both_three_and_five()
     {
         $this->print(self::BUZZ_NUMBER * self::FIZZ_NUMBER * 4)->shouldReturn('FizzBuzz');
     }
@@ -40,5 +40,23 @@ class KataSpec extends ObjectBehavior
         $number = self::BUZZ_NUMBER * self::FIZZ_NUMBER + 1;
 
         $this->print($number)->shouldReturn((string) $number);
+    }
+
+    function it_attempts_to_retrieve_results_from_cache(\Pfazzi\FizzBuzzKata\Cache $cache)
+    {
+        $cache->has(9)->willReturn(true)->shouldBeCalled();
+
+        $cache->get(9)->willReturn('Buzz')->shouldBeCalled();
+
+        $this->print(9)->shouldReturn('Buzz');
+    }
+
+    function it_caches_results(\Pfazzi\FizzBuzzKata\Cache $cache)
+    {
+        $cache->has(9)->willReturn(false)->shouldBeCalled();
+
+        $cache->set(9, '9')->shouldBeCalled();
+
+        $this->print(9)->shouldReturn('9');
     }
 }
--- src/Kata.php
+++ src/Kata.php
@@ -7,22 +7,40 @@ class Kata
     private int $fizzNumber;
     private int $buzzNumber;
 
-    public function __construct(int $fizzNumber, int $buzzNumber)
+    private Cache $cache;
+
+    public function __construct(Cache $cache, int $fizzNumber, int $buzzNumber)
     {
         $this->fizzNumber = $fizzNumber;
         $this->buzzNumber = $buzzNumber;
+        $this->cache = $cache;
     }
 
     public function print(int $number): string
     {
-        if ($number % $this->fizzNumber === 0 && $number % $this->buzzNumber === 0) {
-            return 'FizzBuzz';
-        } elseif ($number % $this->fizzNumber === 0) {
-            return 'Fizz';
-        } elseif ($number % $this->buzzNumber === 0) {
-            return 'Buzz';
-        } else {
-            return (string) $number;
+        if ($this->cache->has($number)) {
+            return $this->cache->get($number);
         }
+
+        $conversionResult = $this->convertNumber($number);
+
+        $this->cache->set($number, $conversionResult);
+
+        return $conversionResult;
     }
+
+    private function convertNumber(int $number): string
+    {
+        if ($number % $this->fizzNumber === 0 && $number % $this->buzzNumber === 0) {
+            $conversionResult = 'FizzBuzz';
+        } elseif ($number % $this->fizzNumber === 0) {
+            $conversionResult = 'Fizz';
+        } elseif ($number % $this->buzzNumber === 0) {
+            $conversionResult = 'Buzz';
+        } else {
+            $conversionResult = (string)$number;
+        }
+
+        return $conversionResult;
+}
 }
